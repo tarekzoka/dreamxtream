1f54d9ee8a0e5e8c2e1b86902811847336a8fb7f
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-20 17:33:07 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,7 +6,8 @@
 ###########################################
 #!/bin/sh
 #
-wget -O /tmp/dreamxtream_0.3-r2.0_armhf.deb "https://github.com/tarekzoka/68/blob/main/dreamxtream_0.3-r2.0_armhf.deb?raw=true"
+wget -O /tmp/dreamxtream_0.3-r2.0_armhf.deb "https://github.com/tarekzoka/dreamxtream
+/blob/main/dreamxtream_0.3-r2.0_armhf.deb?raw=true"
 wait
 apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
 wait
