441ec77ffef9635e51f10aad4156adb6b48a1f6a
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-21 02:53:42 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,39 +1,23 @@
-#!/bin/bash
-######################################################################################
-## Command=wget https://raw.githubusercontent.com/tarekzoka/dreamxtream/main/installer.sh -O - | /bin/sh
-##
-###########################################
-###########################################
 #!/bin/sh
 #
-wget -O /tmp/dreamxtream_0.3-r2.0_armhf.deb "https://github.com/tarekzoka/dreamxtream
-/blob/main/dreamxtream_0.3-r2.0_armhf.deb?raw=true"
-wait
-apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
-wait
-dpkg -i --force-overwrite /tmp/*.deb
-wait
-sleep 2;
-######################################################################################
-##
-echo ""
-# Download and install plugin
-cd /tmp
-set -e
-     wget "$MY_URL/$MY_IPK"
-  wait
-     wget "$MY_URL/$MY_DEB"
+wget -O /tmp/dreamxtream_0.3-r2.0_armhf.deb "https://raw.githubusercontent.com/tarekzoka/dreamxtream/main/dreamxtream_0.3-r2.0_armhf.deb"
 
- if which dpkg > /dev/null 2>&1; then
-		dpkg -i --force-overwrite $MY_DEB; apt-get install -f -y
-	else
-		$OPKGINSTALL $MY_IPK
-	fi
-echo "================================="
-set +e
-cd ..
 wait
-rm -f /tmp/$MY_IPK
-rm -f /tmp/$MY_DEB
-	if [ $? -eq 0 ]; then
-echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
+
+apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
+
+wait
+
+dpkg -i --force-overwrite /tmp/*.deb
+
+wait
+
+rm -r /tmp/dreamxtream_0.3-r2.0_armhf.deb
+
+wait
+
+sleep 2;
+
+exit 0
+
+
